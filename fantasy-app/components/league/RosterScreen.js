400a045d0e49cf27fae5d431781ade150b90e881
Haley Pieratt <haleyhmfr@gmail.com>
2023-05-02 23:18:45 -0600

    clean up

--- fantasy-app/components/league/RosterScreen.js
+++ fantasy-app/components/league/RosterScreen.js
@@ -2,16 +2,15 @@ import React, { Component } from "react";
 import { Button, View, Text } from "react-native";
 import { useNavigation } from '@react-navigation/native';
 import { DataTable } from 'react-native-paper';
+import { ScrollView } from "react-native-web";
 
 export default function RosterScreen({route}) {
   const navigation = useNavigation();
   const roster = route.params;
-  console.log('route: ', roster);
   function rosterList(){
     const rosterKeys = Object.keys(roster);
     return rosterKeys.map((key, index) => {
       return roster[key].map((entry, index) => {
-          console.log(entry.player?.first_name);
             return (
                 <DataTable.Row key={index}>
                     <DataTable.Cell>{key}</DataTable.Cell>
@@ -19,20 +18,13 @@ export default function RosterScreen({route}) {
                     <DataTable.Cell>{entry.player ? entry.player?.location + ' ' + entry.player?.team : null}</DataTable.Cell>
                 </DataTable.Row>              
             );
-            // <Text key={index}>{
-            //   entry.player ? entry.player?.first_name + ' ' + entry.player?.last_name : 'Empty'
-            //   }</Text>);
       });
     });
-    // return roster.map((entry, index) => {
-    //   entry.map((position, index) => {
-    //     return <Text key={index}>{position}</Text>
-    //   })
-    // })
   }
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <ScrollView>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <DataTable>
         <DataTable.Header>
           <DataTable.Title>Position</DataTable.Title>
@@ -43,5 +35,7 @@ export default function RosterScreen({route}) {
 
       </DataTable>
     </View>
+    </ScrollView>
+    
   );
 }
